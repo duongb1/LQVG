b31f9ffd0070f51510eff30e8888d580430d862c
duongb1 <duongm3723002@gstudent.ctu.edu.vn>
2025-09-26 14:21:50 +0700

    Add support for 'm4sardetvg' dataset in build_dataset function

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -18,6 +18,8 @@ def get_coco_api_from_dataset(dataset):
 def build_dataset(dataset_file: str, image_set: str, args):
     if dataset_file == 'rsvg':
         return build_rsvg(image_set, args)
+    if dataset_file == 'm4sardetvg':
+        return build_rsvg(image_set, args)
     if dataset_file == 'rsvg_mm':
         return build_rsvg_mm(image_set, args)
 
